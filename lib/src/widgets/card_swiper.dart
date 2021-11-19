03d87508ed78a8a52c28735cd050410a193bbfa1
Diego Martinez <juandiego337@hotmail.com>
2021-11-19 16:37:10 -0500

    Implementing card swiper in Widget card

--- lib/src/widgets/card_swiper.dart
+++ lib/src/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,30 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final deviceSize = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500.0,
+      height: deviceSize.height * 0.6,
       color: Colors.pink,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK, //La forma en que vamos a mover los cards
+        itemWidth: deviceSize.width * 0.6,
+        itemHeight: deviceSize.height * 0.5,
+        itemBuilder: (BuildContext context, int index){
+            //Debemos regresar un Widget, el que sera utilizado para renderizar la tarjeta
+            return  ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage.assetNetwork(
+                placeholder: 'assets/no-image.jpg',
+                image: 'https://via.placeholder.com/300x400',
+                fit: BoxFit.cover,
+              ),
+            );
+        },
+      ),
     );
   }
 }
