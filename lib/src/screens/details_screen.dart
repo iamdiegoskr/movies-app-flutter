7a53232e62f7361d32e7a8f44dd9afcf8d1e3481
Diego Martinez <juandiego337@hotmail.com>
2021-11-20 18:07:08 -0500

    Poster and title of movie readt

--- lib/src/screens/details_screen.dart
+++ lib/src/screens/details_screen.dart
@@ -13,7 +13,10 @@ class DetailsScreen extends StatelessWidget {
         physics: const BouncingScrollPhysics(
             parent: AlwaysScrollableScrollPhysics()),
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(delegate: SliverChildListDelegate([
+            const _PosterAndTitle()
+          ]))
         ],
       )
     );
@@ -50,6 +53,54 @@ class _CustomAppBar extends StatelessWidget {
           fit: BoxFit.cover,
           placeholder: AssetImage('assets/loading.gif'),
           image: NetworkImage('https://www.xtrafondos.com/wallpapers/paisaje-digital-en-atardecer-5846.jpg')),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 22),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: FadeInImage.assetNetwork(
+              fit: BoxFit.cover,
+              height: 150,
+              placeholder: 'assets/no-image.jpg',
+              image: 'https://via.placeholder.com/200x300'),
+          ),
+          const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Movie title',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.headline5
+                ),
+                const Text(
+                  'Movie original title'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.star_border_outlined, color: Colors.amber, size: 26,),
+                      SizedBox(width: 12,),
+                      Text('4.6')
+                    ],
+                  ),
+                )
+              ],
+            ),
+        ],
       ),
     );
   }
